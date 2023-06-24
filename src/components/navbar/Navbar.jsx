42c35c6ef21fecafc3f9088a8a5932ce1304f642
JohnnyBoi <ninjakai25@gmail.com>
2023-06-24 17:30:24 -0500

    fix mavbar bug when resizing window

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,25 +1,45 @@
 import { NavLink } from 'react-router-dom';
 import logo from '../../../public/images/logo3.png';
 import styles from './Navbar.module.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 export const Navbar = () => {
 
     const [isHamburgerClicked, setIsHamburgerClicked] = useState(false);
-    
+    const [width, setWidth] = useState();
+
+    const getWidth = () => {
+        return window.innerWidth;
+    }
+
+    useEffect(() => {
+        function handleResize() {
+            setWidth(getWidth());
+          }
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, [])
+
     const handleClick = () => {
         setIsHamburgerClicked(!isHamburgerClicked);
     }
 
+    if(isHamburgerClicked && width >= 768) {
+        setIsHamburgerClicked(false);
+    }
+
+
+
+
     return (
         <>
             <nav className={styles.navbar}>
-                <li><NavLink to='/'><img src={logo} /></NavLink></li>
+                <li><NavLink to='/' onClick={handleClick}><img src={logo} /></NavLink></li>
                 <ul className={isHamburgerClicked ? styles.listActive : styles.list}>
-                    <li id={styles.listItem}><NavLink to='cars' className={ ({ isActive }) => isActive ? styles.activeNav : styles.inactiveNav}>Cars</NavLink></li>
-                    <li id={styles.listItem}><NavLink to='team' className={ ({ isActive }) => isActive ? styles.activeNav : styles.inactiveNav}>Our Team</NavLink></li>
-                    <li id={styles.listItem}><NavLink to='contact' className={ ({ isActive }) => isActive ? styles.activeNav : styles.inactiveNav}>Contact</NavLink></li>
-                    <li id={styles.listItem}><NavLink to='about' className={ ({ isActive }) => isActive ? styles.activeNav : styles.inactiveNav}>About</NavLink></li>
+                    <li id={styles.listItem} ><NavLink to='cars' className={ ({ isActive }) => isActive ? styles.activeNav : styles.inactiveNav}>Cars</NavLink></li>
+                    <li id={styles.listItem} ><NavLink to='team' className={ ({ isActive }) => isActive ? styles.activeNav : styles.inactiveNav}>Our Team</NavLink></li>
+                    <li id={styles.listItem} ><NavLink to='contact' className={ ({ isActive }) => isActive ? styles.activeNav : styles.inactiveNav}>Contact</NavLink></li>
+                    <li id={styles.listItem} ><NavLink to='about' className={ ({ isActive }) => isActive ? styles.activeNav : styles.inactiveNav}>About</NavLink></li>
                 </ul>
                 <div className={isHamburgerClicked ? styles.hamburgerActive : styles.hamburger} onClick={handleClick}>
                     <span className={styles.bar}></span>  
