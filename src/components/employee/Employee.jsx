ffd06fb1462af442a062edce0e787bd0889159ed
JohnnyBoi <ninjakai25@gmail.com>
2023-08-20 12:20:08 -0500

    add loading skeleton to pfp images in team page

--- src/components/employee/Employee.jsx
+++ src/components/employee/Employee.jsx
@@ -1,12 +1,19 @@
 import styles from './Employee.module.css';
+import { useState } from 'react';
 
 export const Employee = ({ employee }) => {
 
+    const [isImageLoaded, setIsImageLoaded] = useState(false);
+
+
+    const handleImageLoaded = () => {
+        setIsImageLoaded(true);
+    }
 
     return (
         <>
             <article className={styles.employeeContainer} >
-                <img src={employee.pfp}/>
+                <img className={isImageLoaded ? styles.pfp : styles.pfpLoadingSkeleton} src={employee.pfp} onLoad={handleImageLoaded}/>
                 <h3>{employee.name}</h3>
                 <p>{employee.jobTitle}</p>
                 <div id={styles.moreInfo}>
